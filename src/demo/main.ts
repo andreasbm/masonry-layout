39dfdaf399b10c2d7b0878f16a5a602244a08d87
andreasbm <andmehlsen@gmail.com>
2019-01-27 14:09:33 +0100

    Added more docs.

--- src/demo/main.ts
+++ src/demo/main.ts
@@ -1,22 +1,48 @@
 import { MasonryLayout } from "../lib/masonry-layout";
 
+const $masonry = document.querySelector<MasonryLayout>("#masonry")!;
+const INITIAL_ITEMS_COUNT = 10000;
+
+const $toggleColLockButton = document.querySelector<HTMLButtonElement>("#toggle_col_lock_button")!;
+const $toggleTransitionButton = document.querySelector<HTMLButtonElement>("#toggle_transition_button")!;
+const $addButton = document.querySelector<HTMLButtonElement>("#add_button")!;
+const $colCountButton = document.querySelector<HTMLButtonElement>("#col_count_button")!;
+const $spacingButton = document.querySelector<HTMLButtonElement>("#spacing_button")!;
+
+/**
+ * Returns a random height.
+ */
 function randomHeight (): number {
 	return randomNumberInRange(50, 600);
 }
 
+/**
+ * Returns a random number within a range (included).
+ * @param from
+ * @param to
+ */
 function randomNumberInRange (from: number, to: number): number {
 	return Math.floor(Math.random() * (to - from + 1)) + from;
 }
 
+/**
+ * Returns a random color.
+ */
 function randomColor (): string {
 	const list = ["red", "green", "brown", "yellow", "blue", "orange", "cyan", "pink", "black", "grey"];
 	return list[randomNumberInRange(0, list.length - 1)];
 }
 
+/**
+ * Returns a random image source.
+ */
 function randomImage () {
 	return `https://picsum.photos/${randomNumberInRange(500, 600)}`;
 }
 
+/**
+ * Returns a random item.
+ */
 function randomItem () {
 	return {
 		color: randomColor(),
@@ -25,11 +51,18 @@ function randomItem () {
 	};
 }
 
+/**
+ * Updates the height of an item.
+ * @param $item
+ */
 function updateHeight ($item: HTMLImageElement) {
 	$item.style.height = `${randomHeight()}px`;
 	$masonry.layout();
 }
 
+/**
+ * Adds a new item to the dom.
+ */
 function addNewItem (): HTMLImageElement {
 	const item = randomItem();
 
@@ -44,26 +77,13 @@ function addNewItem (): HTMLImageElement {
 	return $item;
 }
 
-const $masonry = document.querySelector<MasonryLayout>("#masonry")!;
-
-const INITIAL_ITEMS_COUNT = 200;
-
-for (let i = 0; i < INITIAL_ITEMS_COUNT; i++) {
-	addNewItem();
-}
-
-const $toggleColLockButton = document.querySelector<HTMLButtonElement>("#toggle_col_lock_button")!;
-const $toggleTransitionButton = document.querySelector<HTMLButtonElement>("#toggle_transition_button")!;
-const $addButton = document.querySelector<HTMLButtonElement>("#add_button")!;
-const $colCountButton = document.querySelector<HTMLButtonElement>("#col_count_button")!;
-const $spacingButton = document.querySelector<HTMLButtonElement>("#spacing_button")!;
-
-function show ($elem: HTMLElement) {
-	$elem.style.display = `inline-block`;
-}
-
-function hide ($elem: HTMLElement) {
-	$elem.style.display = `none`;
+/**
+ * Initializes the demo.
+ */
+function initialize () {
+	for (let i = 0; i < INITIAL_ITEMS_COUNT; i++) {
+		addNewItem();
+	}
 }
 
 $addButton.addEventListener("click", () => {
@@ -90,3 +110,5 @@ $spacingButton.addEventListener("click", () => {
 	const res = prompt("Enter the amount of spacing (enter nothing for '24')") || "";
 	$masonry.spacing = isNaN(<any>res) ? 24 : parseInt(res);
 });
+
+initialize();
